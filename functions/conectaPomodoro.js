24e4329c62413bb31df1c169c323cb3ccf826fba
Rafaella Ballerini <rafaellaballerini@MacBook-Pro-de-Rafa-2.local>
2023-06-08 15:16:02 -0300

    tentando com youtube

--- functions/conectaPomodoro.js
+++ functions/conectaPomodoro.js
@@ -1,10 +1,16 @@
 const { joinVoiceChannel, createAudioPlayer, createAudioResource, NoSubscriberBehavior} = require('@discordjs/voice');
 const { createReadStream } = require('fs');
+const play = require('play-dl');
 const path = require('path');
+
+let studyTime = 0
 
 module.exports = {
     async conectaPomodoro(tempoPomodoro, interaction){
+        let url= "https://www.youtube.com/watch?v=180ysvIh6kU"
         const audioFilePath = path.join(__dirname, '../assets/yooPausa.mp3');  
+        console.log(audioFilePath)
+        tempoPomodoro = 1
 
         let connection = joinVoiceChannel(
             {
@@ -19,10 +25,24 @@ module.exports = {
             },
         });
 
-        const resource = createAudioResource(createReadStream(audioFilePath));
+        const stream = await play.stream(url);
+
+		const resource = createAudioResource(stream.stream, {
+            inputType: stream.type
+        });
+
+        // const resource = createAudioResource(createReadStream(audioFilePath));
         player.play(resource);
 		connection.subscribe(player);
 
+        // setTimeout(() => {
+        //     player.play(resource);
+        //     connection.subscribe(player);
+        //     console.log("deu timeout")
+        //     studyTime += tempoPomodoro;
+        // }, tempoPomodoro * 60000);
+        
+        console.log(studyTime)
         await interaction.reply('Os estudos começaram!');
     }
 }
